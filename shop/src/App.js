55526b9fa3ca4489e8f8e23934bd6d8059ae8ab8
just-stopyoon <stopjung1025@gmail.com>
2023-08-05 23:17:43 +0900

    blog Finish + shop Start

---
+++ shop/src/App.js
@@ -0,0 +1,55 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Navbar, Container, Nav } from "react-bootstrap";
+import "./App.css";
+import { useState } from "react";
+import data from "./data.js";
+
+function App() {
+  let [shoes] = useState(data);
+
+  return (
+    <div className="app">
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="main-bg"></div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="80%"
+            />
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="80%"
+            />
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="80%"
+            />
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
